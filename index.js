77b1d1df98e9228d3a0892199dc5a1901e5f07b3
Patrick Gotthardt <patrick.gotthardt@trivago.com>
2015-01-16 21:03:29 +0100

    BUGFIX Illegal Symbol to string conversion on io.js

--- index.js
+++ index.js
@@ -74,13 +74,14 @@ var getKeys = (function() {
 
 function getPropertyDescriptors(obj, omitDescriptors) {
     var unique = {},
-        properties = [];
+        properties = [],
+        marker = {};
     for(var proto = obj; proto && proto !== Object.prototype; proto = Object.getPrototypeOf(proto)) {
         var names = getKeys(proto);
         for(var i = 0, len = names.length; i < len; i++) {
             var name = names[i];
-            if(!unique['$$'+name]) {
-                unique['$$'+name] = true;
+            if(unique[name] !== marker) {
+                unique[name] = marker;
                 properties[properties.length] = {name: name, descriptor: omitDescriptors || Object.getOwnPropertyDescriptor(proto, name)};
             }
         }
